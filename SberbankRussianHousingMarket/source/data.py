eb0f66580ff5e252078c3e91ea26c1cd772214af
xierhacker <xierhacker@163.com>
2017-05-02 17:08:24 +0800

    initial commit

--- SberbankRussianHousingMarket/source/data.py
+++ SberbankRussianHousingMarket/source/data.py
@@ -6,10 +6,13 @@ def loadFrame():
     train_frame=pd.read_csv("../data/train.csv")
     test_frame = pd.read_csv("../data/test.csv")
     macro_frame = pd.read_csv("../data/macro.csv",index_col="timestamp")
+    #pop the houseprice from trainDatas
+    train_labels_frame=train_frame.pop("price_doc")
+
 
     #in macro_frame trans index str type to datetime type
     new_index = pd.to_datetime(macro_frame.index)
     macro_frame = macro_frame.reindex(new_index)
 
-    return train_frame,test_frame,macro_frame
+    return train_frame,train_labels_frame,test_frame,macro_frame
 
