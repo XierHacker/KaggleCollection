586c4181fed092c078cd0d149e35b4e8bebdca1f
xierhacker <xierhacker@163.com>
2017-05-07 14:15:14 +0800

    initial commit

--- DigitRecognizer/source/data.py
+++ DigitRecognizer/source/data.py
@@ -1,9 +1,18 @@
+'''
+this file is used to load data and do some basic transform opration.
+if you want see more about the data.you can open the dataExplore.ipynb file in this folder.
+'''
+
 import pandas as pd
 import numpy as np
 
 def loadFrame():
     train_frame=pd.read_csv("../data/train.csv")
     test_frame=pd.read_csv("../data/test.csv")
-    return train_frame,test_frame
+    #pop the labels
+    train_labels_frame=train_frame.pop("label")
+
+    return train_frame,train_labels_frame,test_frame
+
 
 
